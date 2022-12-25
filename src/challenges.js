4304ebe84d2307561df6c26206d3d7c52736ce0b
HigoJhon <higojhon98@gmail.com>
2022-12-25 12:53:06 -0300

    tarefa 10

--- src/challenges.js
+++ src/challenges.js
@@ -9,34 +9,32 @@ function compareTrue(comparador, comparador2) {
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
-  let resultado = (base * height) / 2
-  return resultado
+  let resultado = (base * height) / 2;
+  return resultado;
 }
 
 // Desafio 3
 function splitSentence(text) {
   // seu código aqui
-  let frase = text.split(" ");
-  return frase
+  let frase = text.split(' ');
+  return frase;
 }
 
 // Desafio 4
 function concatName(nomes) {
-
   let a = nomes[0];
   let d = nomes[nomes.length - 1];
-  let resultado = d + ", " + a;
+  let resultado = `${d}, ${a}`;
   return resultado;
-
 }
 // Desafio 5
 function footballPoints(win, ties) {
   // seu código aqui
-  let vitoria = win * 3
-  let empate = ties * 1
+  let vitoria = win * 3;
+  let empate = ties * 1;
 
-  resultado = vitoria + empate
-  return resultado
+  const resultado = vitoria + empate;
+  return resultado;
 }
 
 // Desafio 6
@@ -45,7 +43,7 @@ function highestCount(numeros) {
   let maior = numeros[0];
   for (let index = 0; index < numeros.length; index += 1) {
     if (numeros[index] > maior) {
-      maior = numeros[index]
+      maior = numeros[index];
     }
   }
 
@@ -55,7 +53,7 @@ function highestCount(numeros) {
       resultado += 1;
     }
   }
-  return resultado
+  return resultado;
 }
 
 // Desafio 7
@@ -66,29 +64,28 @@ function catAndMouse(mouse, cat1, cat2) {
   let gato2 = Math.abs(cat2 - mouse);
 
   if (gato1 < gato2) {
-    resultado = "cat1";
+    resultado = 'cat1';
   } else if (gato2 < gato1) {
-    resultado = "cat2";
+    resultado = 'cat2';
   } else {
-    resultado = "os gatos trombam e o rato foge";
+    resultado = 'os gatos trombam e o rato foge';
   }
-  return resultado
+  return resultado;
 }
 
 // Desafio 8
 function fizzBuzz(nume) {
-  // seu código aqui 
+  // seu código aqui
   let resultado = [];
   for (let index = 0; index < nume.length; index += 1) {
-
     if (nume[index] % 3 === 0 && nume[index] % 5 === 0) {
-      resultado.push("fizzBuzz");
+      resultado.push('fizzBuzz');
     } else if (nume[index] % 5 === 0) {
-      resultado.push("buzz");
+      resultado.push('buzz');
     } else if (nume[index] % 3 === 0) {
-      resultado.push("fizz");
+      resultado.push('fizz');
     } else {
-      resultado.push("bug!");
+      resultado.push('bug!');
     }
   }
   return resultado;
@@ -96,22 +93,33 @@ function fizzBuzz(nume) {
 
 // Desafio 9
 function encode(texto) {
-  // seu código aqui 
+  // seu código aqui
   // assistir o video da Ana Cardoso da Trybe
- let letra = texto.replace(/a/g, "1").replace(/e/g, "2").replace(/i/g, "3").replace(/o/g, "4").replace(/u/g, "5");
- return letra
+  let letra = texto.replace(/a/g, '1').replace(/e/g, '2').replace(/i/g, '3').replace(/o/g, '4')
+    .replace(/u/g, '5');
+  return letra;
 }
- 
+
 function decode(texto) {
   // seu código aqui
-  let letra1 = texto.replace(/1/g, "a").replace(/2/g, "e").replace(/3/g, "i").replace(/4/g, "o").replace(/5/, "u"); 
-  return letra1
- 
+  let letra1 = texto.replace(/1/g, 'a').replace(/2/g, 'e').replace(/3/g, 'i').replace(/4/g, 'o')
+    .replace(/5/, 'u');
+  return letra1;
 }
 
 // Desafio 10
-function techList() {
+function techList(tech, name) {
   // seu código aqui
+  if (tech.length === 0) return 'Vazio!';
+  tech.sort();
+  let array = [];
+  for (const valor of tech) {
+    array.push({
+      tech: `${valor}`,
+      name: `${name}`,
+    });
+  }
+  return array;
 }
 
 module.exports = {
